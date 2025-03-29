b2fd4703d2113fb429859a1b5e70eda59c4496e8
Luis Cardenas <luis24.gael@gmail.com>
2025-03-28 18:13:49 -0600

    V29

--- rec.py
+++ rec.py
@@ -102,7 +102,7 @@ def receive_data():
             
             # Limpiar flags
             #write_register(REG_IRQ_FLAGS, 0xFF)
-            #write_register(REG_FIFO_ADDR_PTR, 0x00)
+            write_register(REG_FIFO_ADDR_PTR, 0x00)
             #return data
         time.sleep(0.1)
         #return None
