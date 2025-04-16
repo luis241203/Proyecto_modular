9da9b9dd6c1bc023f33c232034c95997b7cbaa8f
Luis Cardenas <luis24.gael@gmail.com>
2025-04-16 00:14:37 -0600

    adicion_ejemplo_de_slider

--- prueba_tkinter.py
+++ prueba_tkinter.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 
 root = tk.Tk()
 root.title("Prueba Tkinter")
@@ -6,5 +7,10 @@ root.geometry("400x300")
 
 label = tk.Label(root, text="¡Hola desde Tkinter!")
 label.pack(pady=20)
+scale = Scale(root, from_=0, to=100, orient=HORIZONTAL)
+scale.pack()
+
+# Para obtener el valor:
+Button(root, text="Obtener valor", command=lambda: print(scale.get())).pack()
 
 root.mainloop()
