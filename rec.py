202389a0cb9aaab9f07998864d3429aa51c11cc5
Luis Cardenas <luis24.gael@gmail.com>
2025-04-19 15:19:05 -0600

    correcion en recepcion V2

--- rec.py
+++ rec.py
@@ -111,20 +111,22 @@ def lora_recibido():
 
 if __name__ == "__main__":
     try:
+        # Configurar DIO0 como entrada (para interrupción)
+        GPIO.setup(DIO0_PIN, GPIO.IN)
+        
         if init_lora():
-            print("Esperando datos en 433 MHz...")
+            print("Esperando datos...", end='\r')
             while True:
-                if lora_recibido():
-                    data = receive_data()
-                    if data:
-                        print(f"Datos recibidos: {bytes(data).decode('latin-1')}")
-                    # Limpiar flags y volver a modo RX
-                    write_register(REG_IRQ_FLAGS, 0xFF)
-                    write_register(REG_OP_MODE, 0x85)
-                time.sleep(0.1)
+
+                write_register(REG_OP_MODE, 0x85)
+                print("Paquete válido recibido!")
+                data = receive_data()
+                #write_register(REG_IRQ_FLAGS, 0xFF)
+                time.sleep(0.5)  # Pequeña pausa para evitar sobrecarga
                 
     except KeyboardInterrupt:
         print("Recepción detenida")
     finally:
         spi.close()
         GPIO.cleanup()
+
